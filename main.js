da31d5eb0e09448391826faba3eb9cb5c888ad9d
Harsh Kumar Das <127042711+KDHarsh24@users.noreply.github.com>
2023-08-20 21:12:17 +0530

    Landing Done justsome text left

--- main.js
+++ main.js
@@ -145,3 +145,51 @@ function createEye(selector){
         rotateTo,
     };
 }
+
+const canvas = document.querySelector('.draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = "#BADASS"
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 75;
+let isDrawing = false;
+let lastX = 10;
+let lastY = 10;
+let hue = 0;
+
+let direction = true;
+function draw(e){
+    if(!isDrawing)
+        return;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    
+    hue++;
+    if(hue> 360)
+        hue = 0;
+}
+function clearCanvas(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+canvas.addEventListener("mousedown", (e)=>{
+    isDrawing = true;
+    [lastX, lastY]= [e.offsetX, e.offsetY];
+        
+});
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", ()=>{
+    isDrawing = false;
+    clearCanvas();
+});
+canvas.addEventListener("mouseout", ()=>{
+    isDrawing = false;
+    clearCanvas();
+});
